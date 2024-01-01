9ac6d628001254b8e29efa1a6123f83494af4e26
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-01 15:00:47 -0300

    feat: adding new styling to the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
-const Button = () => {
-  return(
-    <button>Click me!</button>
-  )
+const Button = ({ label, iconURL }) => {
+  return (
+    <button className="flex justify-center items-center gap-2 bg-coral-red border-coral-red text-white text-lg px-7 py-4 rounded-full font-montserrat leading-none ">
+      {label}
+      {iconURL && <img src={iconURL} alt={label} className="w-5 h-5 ml-2" />}
+    </button>
+  );
 };
 
 export default Button;

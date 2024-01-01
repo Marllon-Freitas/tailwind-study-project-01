2499b59f816329bad24d49364e0400662aa4694f
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-01 15:31:31 -0300

    feat: adding the styling and the statistics section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from '../components/Button';
 import { arrowRight } from '../assets/icons';
+import { statistics } from '../constants';
 
 const Hero = () => {
   return (
@@ -8,19 +9,33 @@ const Hero = () => {
       className="max-container w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collections</p>
-        <h1>
-          <span>The New Arrival</span>
+        <p className="text-coral-red font-montserrat text-xl">
+          Our Summer Collections
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p>
+        <p className="mt-6 text-slate-gray text-lg leading-8 mb-14 font-montserrat font-medium max-sm:text-center">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
           nisi vitae iure recusandae rem aliquam voluptatem in earum ab quaerat?
           Nulla sint quibusdam nobis est iure quidem! Repellendus, velit
           tenetur.
         </p>
         <Button label="Shop now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((item) => (
+            <div key={item.id} data-aos="fade-up" data-aos-delay="600">
+              <p className="text-4xl font-bold font-palanquin">{item.value}</p>
+              <p className="text-xl text-slate-gray leading-7 font-montserrat">
+                {item.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );

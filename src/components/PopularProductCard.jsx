e72b077789d2baf11a319303ed2f2d0a3e3e8df4
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-05 07:27:23 -0300

    feat: finishing the popular products section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { star } from '../assets/icons';
 
-const PopularProductCard = () => {
-  return <div>PopularProductCard</div>;
+const PopularProductCard = ({ product }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:full">
+      <img
+        src={product.imgURL}
+        alt={product.name}
+        className="w-[280px] h-[280px]"
+      />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+          ({product.rating.toFixed(1)})
+        </p>
+      </div>
+
+      <h3 className="mt-2 text-2xl font-palanquin font-semibold leading-normal">
+        {product.name}
+      </h3>
+      <p className="mt-2 text-2lx font-montserrat leading-normal">
+        {product.price}
+      </p>
+    </div>
+  );
 };
 
 export default PopularProductCard;

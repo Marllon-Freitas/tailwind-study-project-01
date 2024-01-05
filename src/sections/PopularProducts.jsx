fdcacfe1dc2c9a9adda5b7dc5d119b03f4cb3ae4
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-05 07:15:44 -0300

    feat: adding popular products section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,29 @@
+import PopularProductCard from '../components/PopularProductCard';
+import { products } from '../constants';
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col gap-5 justify-start">
+        <h2 className="text-4xl font-bold font-palanquin">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg font-montserrat text-slate-gray mt-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptatum, voluptatibus, quibusdam, quia voluptates voluptate
+          voluptatem quos doloribus quod quas? Quisquam voluptatum,
+          voluptatibus, quibusdam, quia voluptates voluptate voluptatem quos
+          doloribus quod
+        </p>
+      </div>
+
+      <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
